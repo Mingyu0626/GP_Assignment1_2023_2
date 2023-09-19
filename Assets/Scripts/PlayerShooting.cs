b5cfcc108fc4fda9ead6a752ef4723b3c5e3a8f1
Mingyu Choi <mg010626@naver.com>
2023-09-19 21:41:49 +0900

    Implements Shotgunmode

--- Assets/Scripts/PlayerShooting.cs
+++ Assets/Scripts/PlayerShooting.cs
@@ -6,6 +6,7 @@ public class PlayerShooting : MonoBehaviour
 {
     public GameObject prefab;
     public GameObject shootPoint;
+    public bool isShotgunMode = false;
 
     // Start is called before the first frame update
     void Start()
@@ -16,13 +17,28 @@ public class PlayerShooting : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            if (!isShotgunMode) isShotgunMode = true;
+            else isShotgunMode = false;
+        }
+
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
             Instantiate(prefab, transform.position, transform.rotation);
+            if (isShotgunMode)
+            {
+                GameObject clone1 = Instantiate(prefab);
+                clone1.transform.position = transform.position;
+                clone1.transform.rotation = transform.rotation;
+                clone1.transform.Translate(2, 0, 0);
 
-            GameObject clone = Instantiate(prefab);
-            clone.transform.position = transform.position;
-            clone.transform.rotation = transform.rotation;
+                GameObject clone2 = Instantiate(prefab);
+                clone2.transform.position = transform.position;
+                clone2.transform.rotation = transform.rotation;
+                clone2.transform.Translate(-2, 0, 0);
+
+            }
 
         }
     }
