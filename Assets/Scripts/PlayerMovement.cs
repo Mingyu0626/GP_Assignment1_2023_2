34aa4a3346c43de7f6fd871bf134db0701132653
Mingyu Choi <mg010626@naver.com>
2023-09-26 14:11:23 +0900

    Change Input System(In progress)

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,69 +1,92 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.InputSystem;
 using UnityEngine;
 
 public class PlayerBehavior : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        Cursor.visible = false;
-        Cursor.lockState = CursorLockMode.Locked;
-    }
     public bool isDash = false;
     public float speed;
     public float rotationSpeed;
-    // Update is called once per frame
-    void Update()
+
+    private Rigidbody rb;
+    private Vector2 movementValue;
+    private float lookValue;
+
+    private void Awake()
     {
-        if (Input.GetKey(KeyCode.W))
-        {
-            transform.Translate(0, 0, speed * Time.deltaTime);
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            transform.Translate(0, 0, -speed * Time.deltaTime);
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.Translate(-speed * Time.deltaTime, 0, 0);
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.Translate(speed * Time.deltaTime, 0, 0);
-        }
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
 
-        // 대쉬 모드 ON
-        if (Input.GetKey(KeyCode.Z))
-        {
-            if (!isDash)
-            {
-                speed = speed * 2;
-                isDash = true;
-            }
-        }
-
-        // 대쉬 모드 OFF
-        if (Input.GetKey(KeyCode.C))
-        {
-            if (isDash)
-            {
-                speed = speed / 2;
-                isDash = false;
-            }
-        }
-
-        // 플레이어(탱크)의 반시계 방향 회전
-        if (Input.GetKey(KeyCode.Q))
-        {
-            transform.Rotate(0, -10 * rotationSpeed * Time.deltaTime, 0);
-        }
-
-        // 플레이어(탱크)의 시계 방향 회전
-        if (Input.GetKey(KeyCode.E))
-        {
-            transform.Rotate(0, 10 * rotationSpeed * Time.deltaTime, 0);
-        }
+        rb = GetComponent<Rigidbody>();
     }
 
+    public void OnMove(InputValue value)
+    {
+        movementValue = value.Get<Vector2>() * speed;
+    }
+
+    public void OnLook(InputValue value)
+    {
+        lookValue = value.Get<Vector2>().x * rotationSpeed;
+    }
+
+    void Update()
+    {
+        rb.AddRelativeForce(
+            movementValue.x * Time.deltaTime, 
+            0,
+            movementValue.y * Time.deltaTime);
+
+        rb.AddRelativeTorque(0, lookValue * Time.deltaTime, 0);
+
+        //if (Input.GetKey(KeyCode.W))
+        //{
+        //    transform.Translate(0, 0, speed * Time.deltaTime);
+        //}
+        //if (Input.GetKey(KeyCode.S))
+        //{
+        //    transform.Translate(0, 0, -speed * Time.deltaTime);
+        //}
+        //if (Input.GetKey(KeyCode.A))
+        //{
+        //    transform.Translate(-speed * Time.deltaTime, 0, 0);
+        //}
+        //if (Input.GetKey(KeyCode.D))
+        //{
+        //    transform.Translate(speed * Time.deltaTime, 0, 0);
+        //}
+
+        //// 대쉬 모드 ON
+        //if (Input.GetKey(KeyCode.Z))
+        //{
+        //    if (!isDash)
+        //    {
+        //        speed = speed * 2;
+        //        isDash = true;
+        //    }
+        //}
+
+        //// 대쉬 모드 OFF
+        //if (Input.GetKey(KeyCode.C))
+        //{
+        //    if (isDash)
+        //    {
+        //        speed = speed / 2;
+        //        isDash = false;
+        //    }
+        //}
+
+        //// 플레이어(탱크)의 반시계 방향 회전
+        //if (Input.GetKey(KeyCode.Q))
+        //{
+        //    transform.Rotate(0, -10 * rotationSpeed * Time.deltaTime, 0);
+        //}
+
+        //// 플레이어(탱크)의 시계 방향 회전
+        //if (Input.GetKey(KeyCode.E))
+        //{
+        //    transform.Rotate(0, 10 * rotationSpeed * Time.deltaTime, 0);
+        //}
+    }
 }
--- Assets/Scripts/PlayerShooting.cs
+++ Assets/Scripts/PlayerShooting.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.InputSystem;
 using UnityEngine;
 
 public class PlayerShooting : MonoBehaviour
@@ -8,27 +9,21 @@ public class PlayerShooting : MonoBehaviour
     public GameObject shootPoint;
     public bool isShotgunMode = false;
 
-    // Start is called before the first frame update
-    void Start()
+    public void OnFire(InputValue value)
     {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.LeftShift))
+        if (value.isPressed)
         {
-            if (!isShotgunMode) isShotgunMode = true;
-            else isShotgunMode = false;
-        }
-
-        if (Input.GetKeyDown(KeyCode.Mouse0))
-        {
-            Instantiate(prefab, shootPoint.transform.position, shootPoint.transform.rotation);
-
-            if (isShotgunMode)
+            if (!isShotgunMode)
             {
+                GameObject clone = Instantiate(prefab);
+
+                clone.transform.position = shootPoint.transform.position;
+                clone.transform.rotation = shootPoint.transform.rotation;
+            }
+            else
+            {
+                Instantiate(prefab, shootPoint.transform.position, shootPoint.transform.rotation);
+
                 GameObject clone1 = Instantiate(prefab);
                 clone1.transform.position = shootPoint.transform.position;
                 clone1.transform.rotation = shootPoint.transform.rotation;
@@ -39,8 +34,17 @@ public class PlayerShooting : MonoBehaviour
                 clone2.transform.rotation = shootPoint.transform.rotation;
                 clone2.transform.Translate(-3, 0, 0);
 
-            }
 
+            }
+        }
+    }
+
+    public void OnChangeMode(InputValue value)
+    {
+        if (value.isPressed)
+        {
+            if (!isShotgunMode) isShotgunMode = true;
+            else isShotgunMode = false;
         }
     }
 }
